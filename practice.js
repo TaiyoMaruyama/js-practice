cfd2dd6a960838c0bdd6cb56848849ecd3e6b5b2
taiyomaruyama <stickytomatobaby0501@gmail.com>
2023-03-09 08:52:28 +0900

    firstCommit

---
+++ practice.js
@@ -0,0 +1,12 @@
+const getPostData = async () => {
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const jsonData = await response.json();
+    console.log(jsonData.slice(0, 20))
+  }catch(e){
+    console.error(e);
+  }
+
+};
+
+getPostData();
