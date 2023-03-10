cf9e48eb9c8d92e5883541e59e95c868af085db0
taiyomaruyama <stickytomatobaby0501@gmail.com>
2023-03-10 19:12:29 +0900

    change js

--- practice.js
+++ practice.js
@@ -1,13 +1,7 @@
-const getPostData = async () => {
-  try{
-    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-    const jsonData = await response.json();
-    console.log(jsonData.slice(0, 20))
-  }catch(e){
-    console.error(e);
-  }
+function removeOdd(array) {
+  return array.filter(el => {return el % 2 == 0})
 
-  
-};
+}
 
-getPostData();
+array = [1,2,3,4,5,6,7,8]
+console.log(removeOdd(array))
